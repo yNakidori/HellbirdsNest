2e6416f25c02dab163eeb022a20e9e9d27c260db
Naki <leandroreisubi@gmail.com>
2024-08-28 10:26:52 -0300

    base

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./TitleCards.css";
+
+const TitleCards = () => {
+  return <div className="titlecards"></div>;
+};
+
+export default TitleCards;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../Login/Login.css";
+
+const Login = () => {
+  return <div className="login">Login</div>;
+};
+
+export default Login;
