dfcc7474a51251ecff721d377f43757228ba639d
Naki <leandroreisubi@gmail.com>
2024-10-10 23:17:58 -0300

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,30 +3,30 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <section className="py-10 bg-red-50 sm:pt-16 lg:pt-24">
-      <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-        <div className="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
-          <div className="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
+    <section class="py-10 bg-gray-50 sm:pt-16 lg:pt-24">
+      <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div class="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
+          <div class="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
             <img
-              className="w-auto h-9"
+              class="w-auto h-9"
               src="https://cdn.rareblocks.xyz/collection/celebration/images/logo.svg"
-              alt="logo provisório"
+              alt=""
             />
 
-            <p className="text-base leading-relaxed text-gray-600 mt-7">
+            <p class="text-base leading-relaxed text-gray-600 mt-7">
               Amet minim mollit non deserunt ullamco est sit aliqua dolor do
               amet sint. Velit officia consequat duis enim velit mollit.
             </p>
 
-            <ul className="flex items-center space-x-3 mt-9">
+            <ul class="flex items-center space-x-3 mt-9">
               <li>
                 <a
                   href="#"
                   title=""
-                  className="flex items-center justify-center text-white transition-all duration-200 bg-gray-800 rounded-full w-7 h-7 hover:bg-blue-600 focus:bg-blue-600"
+                  class="flex items-center justify-center text-white transition-all duration-200 bg-gray-800 rounded-full w-7 h-7 hover:bg-blue-600 focus:bg-blue-600"
                 >
                   <svg
-                    className="w-4 h-4"
+                    class="w-4 h-4"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     fill="currentColor"
@@ -40,10 +40,10 @@ const Footer = () => {
                 <a
                   href="#"
                   title=""
-                  className="flex items-center justify-center text-white transition-all duration-200 bg-gray-800 rounded-full w-7 h-7 hover:bg-blue-600 focus:bg-blue-600"
+                  class="flex items-center justify-center text-white transition-all duration-200 bg-gray-800 rounded-full w-7 h-7 hover:bg-blue-600 focus:bg-blue-600"
                 >
                   <svg
-                    className="w-4 h-4"
+                    class="w-4 h-4"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     fill="currentColor"
@@ -57,10 +57,10 @@ const Footer = () => {
                 <a
                   href="#"
                   title=""
-                  className="flex items-center justify-center text-white transition-all duration-200 bg-gray-800 rounded-full w-7 h-7 hover:bg-blue-600 focus:bg-blue-600"
+                  class="flex items-center justify-center text-white transition-all duration-200 bg-gray-800 rounded-full w-7 h-7 hover:bg-blue-600 focus:bg-blue-600"
                 >
                   <svg
-                    className="w-4 h-4"
+                    class="w-4 h-4"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     fill="currentColor"
@@ -76,10 +76,10 @@ const Footer = () => {
                 <a
                   href="#"
                   title=""
-                  className="flex items-center justify-center text-white transition-all duration-200 bg-gray-800 rounded-full w-7 h-7 hover:bg-blue-600 focus:bg-blue-600"
+                  class="flex items-center justify-center text-white transition-all duration-200 bg-gray-800 rounded-full w-7 h-7 hover:bg-blue-600 focus:bg-blue-600"
                 >
                   <svg
-                    className="w-4 h-4"
+                    class="w-4 h-4"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     fill="currentColor"
@@ -94,7 +94,145 @@ const Footer = () => {
               </li>
             </ul>
           </div>
+
+          <div>
+            <p class="text-sm font-semibold tracking-widest text-gray-400 uppercase">
+              Company
+            </p>
+
+            <ul class="mt-6 space-y-4">
+              <li>
+                <a
+                  href="#"
+                  title=""
+                  class="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
+                >
+                  {" "}
+                  About{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  title=""
+                  class="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
+                >
+                  {" "}
+                  Features{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  title=""
+                  class="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
+                >
+                  {" "}
+                  Works{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  title=""
+                  class="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
+                >
+                  {" "}
+                  Career{" "}
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <p class="text-sm font-semibold tracking-widest text-gray-400 uppercase">
+              Help
+            </p>
+
+            <ul class="mt-6 space-y-4">
+              <li>
+                <a
+                  href="#"
+                  title=""
+                  class="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
+                >
+                  {" "}
+                  Customer Support{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  title=""
+                  class="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
+                >
+                  {" "}
+                  Delivery Details{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  title=""
+                  class="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
+                >
+                  {" "}
+                  Terms & Conditions{" "}
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  title=""
+                  class="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"
+                >
+                  {" "}
+                  Privacy Policy{" "}
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="col-span-2 md:col-span-1 lg:col-span-2 lg:pl-8">
+            <p class="text-sm font-semibold tracking-widest text-gray-400 uppercase">
+              Subscribe to newsletter
+            </p>
+
+            <form action="#" method="POST" class="mt-6">
+              <div>
+                <label for="email" class="sr-only">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Enter your email"
+                  class="block w-full p-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600"
+                />
+              </div>
+
+              <button
+                type="submit"
+                class="inline-flex items-center justify-center px-6 py-4 mt-3 font-semibold text-white transition-all duration-200 bg-blue-600 rounded-md hover:bg-blue-700 focus:bg-blue-700"
+              >
+                Subscribe
+              </button>
+            </form>
+          </div>
         </div>
+
+        <hr class="mt-16 mb-10 border-gray-200" />
+
+        <p class="text-sm text-center text-gray-600">
+          © Copyright 2021, All Rights Reserved by Postcraft
+        </p>
       </div>
     </section>
   );
